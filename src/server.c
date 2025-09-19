fcc03ee3d422e7d9e948afa65f462c3d8256aa26
SalOrak <99770300+SalOrak@users.noreply.github.com>
2025-09-19 16:31:01 +0200

    feat(server): basic & polling based TCP server

--- src/server.c
+++ src/server.c
@@ -1,12 +1,17 @@
+#define _GNU_SOURCE
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <string.h>
+#include <poll.h>
 
-#define BIND_ADDR "0.0.0.0"
+#define BIND_ADDR "127.0.0.1"
 #define BIND_PORT 8080
+#define MAX 80
+#define MAX_NUM_CLIENTS 10
 
 #define reterr(ERR) \
         do { \
@@ -18,39 +23,87 @@
 // Global error message
 const char *err_msg = NULL;
 
-void handle_connection(int connfd){
-    // char buffer[1024] = "Hello world\n\0";
-    char rd_buffer[32];
-    int rd_ret;
+// Basic read/write connection. Its blocking.
+int handle_connection(int connfd){
+    char rd_buffer[MAX]; 
+    int read_count;
+    int i;
 
+    printf("[CLIENT]: [%d] Handling connection\n", connfd);
     for(;;){
-        // write(connfd, buffer, strlen(buffer));
-        do {
-            rd_ret = read(connfd,rd_buffer, 32);
-        } while(rd_ret > 0);
-        printf("Return value is %d\n", rd_ret);
-        printf(rd_buffer);
-        close(connfd);
-        return;
+        bzero(rd_buffer, MAX);
+
+        // Read from the client
+        read_count = recv(connfd, rd_buffer, sizeof(rd_buffer), 0);
+
+        // Print from the client
+        printf("[CLIENT] says: %s\n", rd_buffer);
+
+        if (strncasecmp("EXIT",rd_buffer, 4) == 0) break;
+
+        // Send message from CLi
+        bzero(rd_buffer, MAX);
+        i = 0;
+        while( (rd_buffer[i++] = getchar()) != '\n'){};
+        write(connfd,rd_buffer, sizeof(rd_buffer));
+    }
+    printf("[CLIENT]: [%d] Closing connection\n", connfd);
+    close(connfd);
+    return 1;
+}
+
+
+/// Polling based connections. "Asynchronous" per nature. Nonblocking.
+void handle_clients(struct pollfd *clients, int num_clients){
+    char buf[1024]; 
+    short rev;
+
+    for (int i = 0 ; i < MAX_NUM_CLIENTS && num_clients > 0; i ++){
+        if (clients[i].fd <= 0 ) continue;
+
+        // Zero-out read buffer
+        bzero(buf, sizeof(buf));
+
+
+        rev = clients[i].revents;
+        // React to Events from each client
+        if (rev &  POLLERR || rev & POLLHUP || rev & POLLRDHUP || rev & POLLPRI) {
+            printf("[EVENT] Client %d closing connection...\n", clients[i].fd);
+            close(clients[i].fd);
+            printf("[EVENT] Client %d is closed\n", clients[i].fd);
+            clients[i].fd = 0;
+        } else if (rev & POLLIN) {
+            read(clients[i].fd, buf, sizeof(buf));
+            if (strlen(buf) == 0) continue;
+            printf("[EVENT] Client %d says %s\n", clients[i].fd, buf);
+        } else if (rev & POLLOUT){}
+        num_clients--;
     }
 }
+
+
 
 
 int main()
 {
     struct in_addr addr;
+    struct sockaddr_in srv_addr;
+    struct sockaddr_in client_addr;
+    int sockfd;
+    int connfd;
+    int num_clients;
+    struct pollfd *clients;
 
     if (inet_aton(BIND_ADDR, &addr) == 0){
         fprintf(stderr, "Invalid address %s\n", BIND_ADDR);
         return 1;
     }
 
-    struct sockaddr_in srv_addr = {0};
     srv_addr.sin_family = AF_INET;
     srv_addr.sin_addr = addr;
     srv_addr.sin_port = htons(BIND_PORT);
 
-    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    sockfd = socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK, 0);
     if (sockfd == 1)
         reterr(err_msg);
 
@@ -60,21 +113,35 @@ int main()
     if (listen(sockfd, 100) != 0)
         reterr(err_msg);
 
+    clients = malloc(sizeof(struct pollfd) * MAX_NUM_CLIENTS);
+
+    socklen_t client_len = sizeof(struct sockaddr_in);
+
+    printf("Waiting to accept connection...\n");
+
     while(1){
-        printf("Waiting to accept connection...\n");
-        struct sockaddr_in client_addr;
-        socklen_t client_len = sizeof(struct sockaddr_in);
+        num_clients = poll(clients, (nfds_t ) MAX_NUM_CLIENTS, 200);
 
-        int connfd = accept(sockfd, (struct sockaddr*)&client_addr, &client_len);
+        handle_clients(clients, num_clients);
 
+        connfd = accept(sockfd, (struct sockaddr*)&client_addr, &client_len);
         if (connfd == -1)
-            reterr(err_msg);
-        printf("Client (socket %d) connected. \n", connfd);
-        printf("[INFO] Client address: %s\n", inet_ntoa(client_addr.sin_addr));
-        printf("[INFO] Client port: %d\n", ntohs(client_addr.sin_port));
+            continue;
 
-        handle_connection(connfd);
+        for (int i = 0; i < MAX_NUM_CLIENTS; i++){
+            if (clients[i].fd <= 0) {
+                printf("[INFO] Client connect at FD %d set at position %d\n", connfd, i);
+
+                clients[i].fd = connfd;
+                clients[i].events = POLLIN | POLLOUT | POLLHUP | POLLRDHUP ;
+
+                break;
+            }
+        }
     };
+
+    close(sockfd);
+    free(clients);
 
     return 0;
 }
